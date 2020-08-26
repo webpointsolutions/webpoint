3396590626e46189061d8de65fd322da28a740de
PrabhakarShrestha <shrestha.prabhakar440@gmail.com>
2020-08-26 15:24:40 +0545

    404, privacy policy and contacts page design added

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -6,7 +6,61 @@ import SEO from "../components/seo"
 const Contact = () => (
   <Layout>
     <SEO title="Contact" />
-    <h1>Contact page</h1>
+    <div className="container-fluid privacy-policy">
+      <div className="row justify-content-center">
+        <div className="col-10">
+          <div className="contact">
+            <h1>We’d like to work together</h1>
+            <div className="form-wrap">
+              <h3>Drop us a line</h3>
+              <p>
+                Feel free to fill out this form to just say hi or start a new
+                project.
+              </p>
+              <form>
+                <div className="d-flex justify-content-between">
+                  <input placeholder="Name" type="text" name="name" />
+                  <div class="input-wrapper">
+                    <input
+                      id="firstName"
+                      name="fname"
+                      type="email"
+                      maxlength="50"
+                      required
+                    />
+                    <span class="placeholder" data-placeholder="Email"></span>
+                  </div>
+                </div>
+                <div class="input-wrapper textarea-wrap">
+                  <textarea
+                    name="message"
+                    rows="1"
+                    cols="30"
+                    required
+                  ></textarea>
+                  <span
+                    class="placeholder"
+                    data-placeholder="Tell us about your project"
+                  ></span>
+                </div>
+                <div class="round">
+                  <input type="checkbox" id="checkbox" />
+                  <label for="checkbox">
+                    <span></span>I have read and agree with{" "}
+                    <a href="">Privacy Policy</a>
+                  </label>
+                </div>
+                <button>Send It</button>
+              </form>
+              <div className="contact-btm">
+                <p>or just write to us</p>
+                <a href="mailto:sales@webpoint.io">sales@webpoint.io</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -6,7 +6,98 @@ import SEO from "../components/seo"
 const PrivacyPolicy = () => (
   <Layout>
     <SEO title="Privacy Policy" />
-    <h1>Privacy Policy page</h1>
+    <div className="container-fluid privacy-policy">
+      <div className="row justify-content-center">
+        <div className="col-10">
+          <div className="privacy-policy-wrap">
+            <h1>Privacy Policy</h1>
+            <p>
+              WebPoint Solutions LLC (https://webpoint.io./) is committed to
+              protecting the privacy of all of our website visitors. In this
+              privacy policy, you can familiarize yourself with the different
+              kinds of information that we collect and how we use it. If you
+              have any questions regarding how WebPoint Solutions LLC protects
+              your privacy, feel free to contact us via phone or email:
+            </p>
+            <p>
+              Email:info@webpoint.io <br />
+              Phone: +1 (404) 755-6225.
+            </p>
+            <h3>Log Files</h3>
+            <p>
+              WebPoint Solutions uses standard procedures when we use log files.
+              These are the files that log when a visitor visits our website.
+              Every hosting company does this as a way to provide hosting
+              analytics. The important information contained in log files
+              includes IP addresses, ISPs, date and time stamp, referring/exit
+              pages, browser type, and even how many clicks you made on our
+              website.{" "}
+            </p>
+            <p>
+              You can rest assured that this information is not linked to any
+              identifiable personal information. We use this information solely
+              for the purpose of analyzing trends, administering our website,
+              tracking our visitors' movements while they are on our website,
+              and obtaining demographic information.
+            </p>
+            <h3>Cookies and Beacons</h3>
+            <p>
+              All websites use cookies, so WebPoint Solutions is no exception!
+              Cookies are used by every website to store information, such as
+              visitor preferences and the pages that the visitor visited while
+              on the website. The information that we collect through cookies
+              helps us to optimize your experience when you are on our website.
+              For example, we try to customize our web page content based on our
+              visitors' browser types and other information that we gain through
+              cookies.
+            </p>
+            <p>
+              If you would like to, you can disable or manage cookies through
+              your browser settings. If you would like to learn more about how
+              to manage cookies with the browser that you are using, you will
+              need to find it on your browser's respective website.
+            </p>
+            <h3>Third-Party Privacy Policies</h3>
+            <p>
+              WebPoint Solutions's Privacy Policy does not apply to other
+              advertisers or websites. You will need to consider the individual
+              privacy policies of any respective third-party ad servers,
+              partners, or other websites that we link to. On each respective
+              site is where you will likely find the information about how to
+              opt-out of their options and what their privacy protection
+              practices are.
+            </p>
+            <h3>Children's Information</h3>
+            <p>
+              WebPoint Solutions is very serious about protecting children if
+              and when they land on our website. Of course, we encourage parents
+              always to observe, monitor, and participate in their children's
+              online activities, but we also know that this is not always
+              possible. That is why WebPoint Solutions goes above and beyond to
+              protect the privacy of minors. WebPoint Solutions does not
+              knowingly collect any Personal Identifiable Information from
+              children under the age of 13. However, if you suspect that your
+              child provided this kind of identifiable information on our
+              https://webpoint.io./, then please, contact us ASAP! We will
+              gladly and promptly remove any such information from our records
+              if it has been acquired.{" "}
+            </p>
+            <h3>Online Privacy Policy Only</h3>
+            <p>
+              This privacy policy is limited in scope, as it only applies to our
+              activities online. Our website visitors should know that this
+              policy is not applicable to any information that is obtained via
+              offline channels outside of this website.
+            </p>
+            <h3>Consent</h3>
+            <p>
+              By using https://webpoint.io./, you are consenting to the terms
+              and conditions set forth in this privacy policy.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
