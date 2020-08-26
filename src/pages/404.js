12ba1f47de23dc526a56f5223014090a3030a9c2
PrabhakarShrestha <shrestha.prabhakar440@gmail.com>
2020-08-26 11:55:36 +0545

    404 page design added

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,17 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container-fluid not-found">
+      <div className="row justify-content-center">
+        <div className="col-10">
+          <img src="../../static/images/404.png" alt="404" />
+          <h1>Whoops, that page is gone.</h1>
+          <a href="" className="back-btn">
+            <i className="arr-left"></i>Go Back
+          </a>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
