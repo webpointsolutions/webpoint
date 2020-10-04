9f4f1243d3fde733a5e78877ef6b363b761b3500
James Wilson <james@webpoint.io>
2020-10-04 11:31:11 +0545

    updated seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Work from "../components/Work/work"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Webpoint Soltuions" description="Webpoint is a full service web & mobile applications development company. We specialize in creating clean and modern UI/UX designs and web and app development using the latest technologies." />
     <HeroVideo />
     <Service />
     <Work />
