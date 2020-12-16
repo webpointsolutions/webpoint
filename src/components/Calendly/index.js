05651444ae0a9c2e08225d17b6d9b1f07c048b33
yogesh upreti <yogeshupreti5@gmail.com>
2020-12-16 13:37:15 +0545

    Calendly text changed

--- src/components/Calendly/index.js
+++ src/components/Calendly/index.js
@@ -3,6 +3,6 @@ import { PopupWidget } from 'react-calendly'
 
 export default () => {
     return (
-    <PopupWidget url="https://calendly.com/webpoint/30min?hide_gdpr_banner=1" text={`Book a Free Consultation`}/>
+    <PopupWidget url="https://calendly.com/webpoint/30min?hide_gdpr_banner=1" text="Book a Free Consultation"/>
     )
 }
