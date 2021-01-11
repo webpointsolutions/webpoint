5921c4181866e7fe0db69843fba007221fe66d31
James Wilson <james@webpoint.io>
2021-01-11 18:24:17 +0545

    add seo schema

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,34 @@ import Calendly from '../components/Calendly'
 
 const IndexPage = () => {
   const [animCompleted, setAnimCompleted] = useState(false)
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "Webpoint Solutions, LLC",
+    "image": "https://webpoint.io/images/logo-blue.svg",
+    "url": "https://webpoint.io/",
+    "telephone": "+1 (470) 755-6225",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "255 Arthurs Lane",
+      "addressLocality": "Covington",
+      "addressRegion": "GA",
+      "postalCode": "30016",
+      "addressCountry": "US"
+    },
+    "sameAs": [
+      "https://www.facebook.com/webpointsolutionsllc",
+      "https://twitter.com/WebpointSoluti1",
+      "https://www.linkedin.com/company/webpoint-solutions-llc/"
+    ],
+
+  }
   return (
     <Layout hideFooter={!animCompleted}>
       <SplashScreen onAnimCompleted={() => {
         setAnimCompleted(true)
       }}>
-        <SEO title="Website Design & Development | App Development - Atlanta" description="Webpoint is a leading web and mobile applications development company. We create beautiful UI/UX designs and engaging user experiences" />
+        <SEO title="Website Design & Development | App Development - Atlanta" description="Webpoint is a leading web and mobile applications development company. We create beautiful UI/UX designs and engaging user experiences" schemaMarkup={schema}/>
         <HeroVideo />
         <Service />
         <Work />
