61b39852bb2549fcc4995ff2ae79890af781fd85
yogesh upreti <yogeshupreti5@gmail.com>
2021-01-14 15:32:14 +0545

    Blog api updated to Https

--- src/api/blogs.js
+++ src/api/blogs.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "http://blog.webpoint.io/wp-json/wp/v2/"
+const BASE_URL = "https://blog.webpoint.io/wp-json/wp/v2/"
 
 export const fetchBlogPosts = async () => {
     return await axios.get(`${BASE_URL}posts`)
