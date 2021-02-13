8f956d6228e0731c5161e52c2f92a1fddd9ff2b0
sushant suwal <sushantdsuwal@gmail.com>
2021-02-13 21:23:26 +0545

    fix import

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,175 +1,189 @@
-import React, { useEffect, useState } from 'react'
-import moment from 'moment'
-import Footer from '../components/Footer/footer'
-import Header from '../components/Header/header'
-import SEO from '../components/seo'
-import LazyImage from '../components/FeaturedBlog /lazyImage'
-import { fetchFeaturedMedia, fetchBlogCategories, fetchBlogTags, fetchPostBySlug } from '../api/blogs'
-import Comments from '../components/Comments/Comments'
-import FB from '../../static/images/icons/fb.svg'
-import Twitter from '../../static/images/icons/twitter.svg'
-import './article.scss'
+import React, { useEffect, useState } from "react"
+import moment from "moment"
+import Footer from "../components/Footer/footer"
+import Header from "../components/Header/header"
+import SEO from "../components/seo"
+import LazyImage from "../components/FeaturedBlog/lazyImage"
+import {
+  fetchFeaturedMedia,
+  fetchBlogCategories,
+  fetchBlogTags,
+  fetchPostBySlug,
+} from "../api/blogs"
+import Comments from "../components/Comments/Comments"
+import FB from "../../static/images/icons/fb.svg"
+import Twitter from "../../static/images/icons/twitter.svg"
+import "./article.scss"
 
-const Article = (props) => {
-    const param = new URLSearchParams(props.location.search)
-    const query = param.get('q')
-    const [categories, setCategories] = useState(null)
-    const [tags, setTags] = useState(null)
-    const [blog, setBlog] = useState()
-    const [commentsLoaded, setComments] = useState(0)
-    const [loaded, setLoaded] = useState(false)
-    const [commentOrder, setOrder] = useState("desc")
+const Article = props => {
+  const param = new URLSearchParams(props.location.search)
+  const query = param.get("q")
+  const [categories, setCategories] = useState(null)
+  const [tags, setTags] = useState(null)
+  const [blog, setBlog] = useState()
+  const [commentsLoaded, setComments] = useState(0)
+  const [loaded, setLoaded] = useState(false)
+  const [commentOrder, setOrder] = useState("desc")
 
-    const fetchBlog = async (query, __categories, __tags) => {
-        try {
-            var data = await fetchPostBySlug(query)
-            if (data.status === 200) {
-                let _blog = data.data[0]
-                const _blog_categories = _blog.categories.map(cat => {
-                    return categories.filter(c => c.id === cat)[0].name
-                })
-                const _blog_tags = _blog.tags.map(t => {
-                    return tags.filter(tt => tt.id === t)[0].name
-                })
-                _blog.categories = _blog_categories
-                _blog.tags = _blog_tags
-                console.log(_blog)
-                setBlog(_blog)
-                setLoaded(true)
-            }
-        } catch (e) {
-            console.warn(e)
-        }
+  const fetchBlog = async (query, __categories, __tags) => {
+    try {
+      var data = await fetchPostBySlug(query)
+      if (data.status === 200) {
+        let _blog = data.data[0]
+        const _blog_categories = _blog.categories.map(cat => {
+          return categories.filter(c => c.id === cat)[0].name
+        })
+        const _blog_tags = _blog.tags.map(t => {
+          return tags.filter(tt => tt.id === t)[0].name
+        })
+        _blog.categories = _blog_categories
+        _blog.tags = _blog_tags
+        console.log(_blog)
+        setBlog(_blog)
+        setLoaded(true)
+      }
+    } catch (e) {
+      console.warn(e)
     }
+  }
 
-    const fetchCategories = async () => {
-        const _categories = await fetchBlogCategories()
-        if (_categories.status === 200) {
-            let _categoriesList = _categories.data.map(cat => ({
-                name: cat.name,
-                id: cat.id
-            }))
-            setCategories(_categoriesList)
-        }
+  const fetchCategories = async () => {
+    const _categories = await fetchBlogCategories()
+    if (_categories.status === 200) {
+      let _categoriesList = _categories.data.map(cat => ({
+        name: cat.name,
+        id: cat.id,
+      }))
+      setCategories(_categoriesList)
     }
+  }
 
-    const fetchTags = async () => {
-        const _tags = await fetchBlogTags()
-        if (_tags.status === 200) {
-            let _tagsList = _tags.data.map(tag => ({
-                name: tag.name,
-                slug: tag.slug,
-                id: tag.id
-            }))
-            setTags(_tagsList)
+  const fetchTags = async () => {
+    const _tags = await fetchBlogTags()
+    if (_tags.status === 200) {
+      let _tagsList = _tags.data.map(tag => ({
+        name: tag.name,
+        slug: tag.slug,
+        id: tag.id,
+      }))
+      setTags(_tagsList)
 
-            fetchBlog(query)
-        }
+      fetchBlog(query)
     }
+  }
 
-    useEffect(() => {
-        fetchCategories()
-    }, [])
+  useEffect(() => {
+    fetchCategories()
+  }, [])
 
-    useEffect(() => {
-        if (categories) {
-            fetchTags()
-        }
-    }, [categories])
+  useEffect(() => {
+    if (categories) {
+      fetchTags()
+    }
+  }, [categories])
 
-    useEffect(() => {
-        if (tags) {
-            fetchBlog(query, categories, tags)
-        }
-    }, [categories, tags])
+  useEffect(() => {
+    if (tags) {
+      fetchBlog(query, categories, tags)
+    }
+  }, [categories, tags])
 
-    return (
-        <div>
-            <Header />
-            <SEO title="Article" description="..." />
-            {
-                loaded ?
-                    <>
-                        <div className="d-flex flex-column align-items-center justify-content-center">
-                            <div className="blog-top">
-                                <div className="col-lg-8 top-title-container">
-                                    <div>
-                                        {blog.tags.map((tag, index) => <p key={`top-${index}`} className="blog-tags">{tag}</p>)}
-                                        <div className="dot-div-blog"></div>
-                                        <p className="blog-tags">{moment(blog.date).format("MMMM DD, YYYY")}</p>
-                                        <div className="dot-div-blog"></div>
-                                        <p className="blog-tags">5 min read</p>
-                                    </div>
-                                    <div>
-                                        <p className="blog-tags top-left-item">share article</p>
-                                        <img src={FB} className="blog-top-icon" alt="" />
-                                        <img src={Twitter} className="blog-top-icon" alt="" />
-                                    </div>
-                                </div>
-                                <div className="col-lg-8 top-title-container">
-                                    <p className="b-title">{blog.title.rendered}</p>
-
-                                </div>
-                            </div>
-                            <div className="b-overlay-container">
-                                <div className="featured-image-container">
-                                    <LazyImage type={"id"} value={blog.featured_media} containerClass="featured-image-container" imgClass="feat-image" />
-                                </div>
-                            </div>
-                            <div className="">
-                                <div
-                                    dangerouslySetInnerHTML={{ "__html": blog.content.rendered }}
-                                    className="article-container"
-                                >
-
-                                </div>
-                            </div>
-                            <div className="col-lg-8 tag-holder">
-                                {blog.tags.map(tag => <span>{`#${tag}`}</span>)}
-                            </div>
-                            <div className="col-lg-8 next-btn">
-                                <p>Next</p>
-                            </div>
-                            <div className="comments-holder">
-                                <div className="comments-top col-md-8">
-                                    <p>{`Comments`}<span className="cmt-number">{`(${commentsLoaded})`}</span></p>
-                                    <select
-                                        onChange={e => {
-                                            console.log(e.target.value)
-                                            setOrder(e.target.value)
-                                        }}
-                                    >
-                                        <option value="desc">Newest</option>
-                                        <option value="asc">Oldest</option>
-                                    </select>
-                                </div>
-                                <div className="comments-body col-md-8">
-                                    {blog ?
-                                        <Comments
-                                            postId={blog.id}
-                                            onChangeComments={setComments}
-                                            order={commentOrder}
-                                            /> :
-                                        <div className="loader-container">
-                                            <div className="loader">
-
-                                            </div>
-                                        </div>
-                                    }
-                                </div>
-                            </div>
-                        </div>
-                    </> :
-                    <div className="loader-container">
-                        <div className="loader">
-
-                        </div>
-                    </div>
-            }
-            <Footer />
+  return (
+    <div>
+      <Header />
+      <SEO title="Article" description="..." />
+      {loaded ? (
+        <>
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <div className="blog-top">
+              <div className="col-lg-8 top-title-container">
+                <div>
+                  {blog.tags.map((tag, index) => (
+                    <p key={`top-${index}`} className="blog-tags">
+                      {tag}
+                    </p>
+                  ))}
+                  <div className="dot-div-blog"></div>
+                  <p className="blog-tags">
+                    {moment(blog.date).format("MMMM DD, YYYY")}
+                  </p>
+                  <div className="dot-div-blog"></div>
+                  <p className="blog-tags">5 min read</p>
+                </div>
+                <div>
+                  <p className="blog-tags top-left-item">share article</p>
+                  <img src={FB} className="blog-top-icon" alt="" />
+                  <img src={Twitter} className="blog-top-icon" alt="" />
+                </div>
+              </div>
+              <div className="col-lg-8 top-title-container">
+                <p className="b-title">{blog.title.rendered}</p>
+              </div>
+            </div>
+            <div className="b-overlay-container">
+              <div className="featured-image-container">
+                <LazyImage
+                  type={"id"}
+                  value={blog.featured_media}
+                  containerClass="featured-image-container"
+                  imgClass="feat-image"
+                />
+              </div>
+            </div>
+            <div className="">
+              <div
+                dangerouslySetInnerHTML={{ __html: blog.content.rendered }}
+                className="article-container"
+              ></div>
+            </div>
+            <div className="col-lg-8 tag-holder">
+              {blog.tags.map(tag => (
+                <span>{`#${tag}`}</span>
+              ))}
+            </div>
+            <div className="col-lg-8 next-btn">
+              <p>Next</p>
+            </div>
+            <div className="comments-holder">
+              <div className="comments-top col-md-8">
+                <p>
+                  {`Comments`}
+                  <span className="cmt-number">{`(${commentsLoaded})`}</span>
+                </p>
+                <select
+                  onChange={e => {
+                    console.log(e.target.value)
+                    setOrder(e.target.value)
+                  }}
+                >
+                  <option value="desc">Newest</option>
+                  <option value="asc">Oldest</option>
+                </select>
+              </div>
+              <div className="comments-body col-md-8">
+                {blog ? (
+                  <Comments
+                    postId={blog.id}
+                    onChangeComments={setComments}
+                    order={commentOrder}
+                  />
+                ) : (
+                  <div className="loader-container">
+                    <div className="loader"></div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="loader-container">
+          <div className="loader"></div>
         </div>
-
-    )
+      )}
+      <Footer />
+    </div>
+  )
 }
 
 export default Article
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,175 +1,191 @@
-import React, { useState, useEffect, useRef } from 'react'
-import moment from 'moment'
-import Footer from '../components/Footer/footer'
-import Header from '../components/Header/header'
+import React, { useState, useEffect, useRef } from "react"
+import moment from "moment"
+import Footer from "../components/Footer/footer"
+import Header from "../components/Header/header"
 // import ImageBackground from '../components/ImageBackground'
-import SEO from '../components/seo'
-import { convertHtmlToDom, parseHtml } from '../utils/utils'
-import { fetchBlogCategories, fetchBlogPosts, fetchBlogTags } from '../api/blogs'
-import './blog.scss'
-import LazyImage from '../components/FeaturedBlog /lazyImage'
-import TopBar from '../components/Blogs/topNav'
-import BlogList from '../components/Blogs/blogList'
-import { Link } from 'gatsby'
+import SEO from "../components/seo"
+import { convertHtmlToDom, parseHtml } from "../utils/utils"
+import {
+  fetchBlogCategories,
+  fetchBlogPosts,
+  fetchBlogTags,
+} from "../api/blogs"
+import "./blog.scss"
+import LazyImage from "../components/FeaturedBlog/lazyImage"
+import TopBar from "../components/Blogs/topNav"
+import BlogList from "../components/Blogs/blogList"
+import { Link } from "gatsby"
 
+const Blog = props => {
+  const [loaded, setLoaded] = useState(false)
+  const [categories, setCategories] = useState(null)
+  const [tags, setTags] = useState(null)
+  const [featured, setFeatured] = useState([])
+  const [blogs, setBlogs] = useState([])
+  const [tagChoice, setTagChoice] = useState(null)
+  const [orderChoice, setOrderChoices] = useState(null)
+  const [blogLoading, setBlogLoading] = useState(false)
 
-const Blog = (props) => {
-    const [loaded, setLoaded] = useState(false)
-    const [categories, setCategories] = useState(null)
-    const [tags, setTags] = useState(null)
-    const [featured, setFeatured] = useState([])
-    const [blogs, setBlogs] = useState([])
-    const [tagChoice, setTagChoice] = useState(null)
-    const [orderChoice, setOrderChoices] = useState(null)
-    const [blogLoading, setBlogLoading] = useState(false)
+  const fetchBlogs = async (tag_choice = null, date_order = "desc") => {
+    var b = await fetchBlogPosts(tag_choice, date_order)
+    var blogList = b.data.slice(0, 2).map(bl => {
+      const cat_names = bl.categories.map(c => {
+        return categories.filter(cc => cc.id === c)[0].name
+      })
+      const tag_names = bl.tags.map(t => {
+        return tags.filter(tt => tt.id === t)[0].name
+      })
+      return {
+        title: bl.title.rendered,
+        subTitle: cat_names.join(", "),
+        image:
+          bl.featured_media !== 0
+            ? { type: "id", value: bl.featured_media }
+            : {
+                type: "src",
+                value: parseHtml(bl.content.rendered, "img", "src"),
+              },
+        content: bl.content.rendered,
+        slug: bl.slug,
+        tags: tag_names,
+        date: bl.date,
+      }
+    })
+    return blogList
+  }
 
-    const fetchBlogs = async (tag_choice = null, date_order = "desc") => {
-        var b = await fetchBlogPosts(tag_choice, date_order)
-        var blogList = b.data.slice(0, 2).map(bl => {
-            const cat_names = bl.categories.map(c => {
-                return categories.filter(cc => cc.id === c)[0].name
-            })
-            const tag_names = bl.tags.map(t => {
-                return tags.filter(tt => tt.id === t)[0].name
-            })
-            return {
-                title: bl.title.rendered,
-                subTitle: cat_names.join(", "),
-                image: (bl.featured_media !== 0) ? { type: "id", value: bl.featured_media } : { type: "src", value: parseHtml(bl.content.rendered, "img", "src") },
-                content: bl.content.rendered,
-                slug: bl.slug,
-                tags: tag_names,
-                date: bl.date
-            }
-        })
-        return blogList
+  const fetchInitialList = async () => {
+    const blog_list = await fetchBlogs()
+    setBlogs(blog_list)
+    setFeatured(blog_list[0])
+    setLoaded(true)
+  }
+
+  const fetchSubsequentList = async (tag_choice, order) => {
+    setBlogLoading(true)
+    const blog_list = await fetchBlogs(tag_choice, order)
+    setBlogs(blog_list)
+    setBlogLoading(false)
+  }
+
+  const fetchCategories = async () => {
+    const _categories = await fetchBlogCategories()
+    if (_categories.status === 200) {
+      let _categoriesList = _categories.data.map(cat => ({
+        name: cat.name,
+        id: cat.id,
+      }))
+      setCategories(_categoriesList)
     }
+  }
 
-    const fetchInitialList = async () => {
-        const blog_list = await fetchBlogs()
-        setBlogs(blog_list)
-        setFeatured(blog_list[0])
-        setLoaded(true)
+  const fetchTags = async () => {
+    const _tags = await fetchBlogTags()
+    if (_tags.status === 200) {
+      let _tagsList = _tags.data.map(tag => ({
+        name: tag.name,
+        slug: tag.slug,
+        id: tag.id,
+      }))
+      setTags(_tagsList)
     }
+  }
+  useEffect(() => {
+    fetchTags()
+    fetchCategories()
+  }, [])
 
-    const fetchSubsequentList = async (tag_choice, order) => {
-        setBlogLoading(true)
-        const blog_list = await fetchBlogs(tag_choice, order)
-        setBlogs(blog_list)
-        setBlogLoading(false)
+  useEffect(() => {
+    if (categories && tags) {
+      fetchInitialList()
     }
+  }, [categories, tags])
 
-    const fetchCategories = async () => {
-        const _categories = await fetchBlogCategories()
-        if (_categories.status === 200) {
-            let _categoriesList = _categories.data.map(cat => ({
-                name: cat.name,
-                id: cat.id
-            }))
-            setCategories(_categoriesList)
-        }
+  useEffect(() => {
+    if (loaded) {
+      fetchSubsequentList(tagChoice, orderChoice ? "desc" : "asc")
     }
+  }, [tagChoice, orderChoice, loaded])
 
-    const fetchTags = async () => {
-        const _tags = await fetchBlogTags()
-        if (_tags.status === 200) {
-            let _tagsList = _tags.data.map(tag => ({
-                name: tag.name,
-                slug: tag.slug,
-                id: tag.id
-            }))
-            setTags(_tagsList)
-        }
-    }
-    useEffect(() => {
-        fetchTags()
-        fetchCategories()
-    }, [])
+  console.log(props)
 
-    useEffect(() => {
-        if (categories && tags) {
-            fetchInitialList()
-        }
-    }, [categories, tags])
-
-    useEffect(() => {
-        if (loaded) {
-            fetchSubsequentList(tagChoice, orderChoice ? "desc" : "asc")
-        }
-    }, [tagChoice, orderChoice, loaded])
-
-    console.log(props)
-
-    return (
-        <div>
-            <Header />
-            <SEO title="Webpoint Blog" description="The Webpoint blog focuses on various design and development tutorials. You will find actionable tips and helpful resources on web performance and security, UX/UI Design, best technologies, and much more..."/>
-            <section id="blog-main">
-                <div className="container-fluid">
-                    <div className="row justify-content-center">
-                        <div className="d-flex b-outer">
-                            <div>
-                                {loaded ?
-                                    <div className="d-flex flex-column align-items-center justify-content-center">
-                                        <div className="b-inner">
-                                            <div className="col-lg-8">
-                                                <p className="top-title">Webpoint Blog</p>
-                                            </div>
-                                        </div>
-                                        <div className="b-overlay-container">
-                                            <div className="featured-image-container">
-                                                <LazyImage type={featured.image.type} value={featured.image.value} containerClass="featured-image-container" imgClass="feat-image" />
-                                            </div>
-                                            <div className="b-overlay">
-                                                <div className="col-md-8">
-                                                    <div className="overlay-top">
-                                                        <div className="featured-text">
-                                                            <p>Featured</p>
-                                                        </div>
-                                                        <p className="feat-topic">{featured.tags}</p>
-                                                        <p className="feat-date">{moment(featured.date).format("MMMM DD, YYYY")}</p>
-                                                    </div>
-                                                    <Link to={`/article?q=${featured.slug}`}>
-                                                        <p className="feat-title">{featured.title}</p>
-                                                    </Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-8">
-                                            {/* Header Options */}
-                                            <TopBar
-                                                tags={tags}
-                                                onTopicChange={(t) => {
-                                                    const id = t.id === 0 ? "" : t.id
-                                                    setTagChoice(id)
-                                                }}
-                                                onFilterApplied={() => { }}
-                                            />
-                                            {blogLoading ?
-                                                <div className="loader-container-mini">
-                                                    <div className="loader">
-
-                                                    </div>
-                                                </div>
-                                                :
-                                                <BlogList items={blogs} />
-                                            }
-                                        </div>
-                                    </div> :
-                                    <div className="loader-container">
-                                        <div className="loader">
-
-                                        </div>
-                                    </div>
-                                }
-                            </div>
-                        </div>
+  return (
+    <div>
+      <Header />
+      <SEO
+        title="Webpoint Blog"
+        description="The Webpoint blog focuses on various design and development tutorials. You will find actionable tips and helpful resources on web performance and security, UX/UI Design, best technologies, and much more..."
+      />
+      <section id="blog-main">
+        <div className="container-fluid">
+          <div className="row justify-content-center">
+            <div className="d-flex b-outer">
+              <div>
+                {loaded ? (
+                  <div className="d-flex flex-column align-items-center justify-content-center">
+                    <div className="b-inner">
+                      <div className="col-lg-8">
+                        <p className="top-title">Webpoint Blog</p>
+                      </div>
                     </div>
-                </div>
-            </section>
-            <Footer />
+                    <div className="b-overlay-container">
+                      <div className="featured-image-container">
+                        <LazyImage
+                          type={featured.image.type}
+                          value={featured.image.value}
+                          containerClass="featured-image-container"
+                          imgClass="feat-image"
+                        />
+                      </div>
+                      <div className="b-overlay">
+                        <div className="col-md-8">
+                          <div className="overlay-top">
+                            <div className="featured-text">
+                              <p>Featured</p>
+                            </div>
+                            <p className="feat-topic">{featured.tags}</p>
+                            <p className="feat-date">
+                              {moment(featured.date).format("MMMM DD, YYYY")}
+                            </p>
+                          </div>
+                          <Link to={`/article?q=${featured.slug}`}>
+                            <p className="feat-title">{featured.title}</p>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-lg-8">
+                      {/* Header Options */}
+                      <TopBar
+                        tags={tags}
+                        onTopicChange={t => {
+                          const id = t.id === 0 ? "" : t.id
+                          setTagChoice(id)
+                        }}
+                        onFilterApplied={() => {}}
+                      />
+                      {blogLoading ? (
+                        <div className="loader-container-mini">
+                          <div className="loader"></div>
+                        </div>
+                      ) : (
+                        <BlogList items={blogs} />
+                      )}
+                    </div>
+                  </div>
+                ) : (
+                  <div className="loader-container">
+                    <div className="loader"></div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </section>
+      <Footer />
+    </div>
+  )
 }
 
 export default Blog
