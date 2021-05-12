18d564d167ad6ed209538cfba66609535cf14cca
suren sedai <suren.sedai9@gmail.com>
2021-05-12 14:54:04 +0545

    content heading of lets work together component created and text underline issue of technology component resolved

--- src/components/HeroVideo/heroVideo.js
+++ src/components/HeroVideo/heroVideo.js
@@ -11,41 +11,47 @@ const HeroVideo = () => {
   }, [])
 
   return (
-  <section data-aos="fade-up" className="HeroVideo">
-    <div className="container-fluid">
-      <div className="row justify-content-center">
-        <div className="col-lg-10">
-          <div className="HeroVideo_text">
-            <h1 data-aos="fade-up" data-aos-duration="800">
-              Your Web & Mobile App Development Partner - Empowered By Innovation 
-            </h1>
-            <div data-aos="fade-up" data-aos-duration="1000">
-            <Link to="/contact" className={loaded?"loaded-link":""}>Let’s work together</Link>
+    <section data-aos="fade-up" className="HeroVideo">
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-lg-10">
+            <div className="HeroVideo_text">
+              <h1 data-aos="fade-up" data-aos-duration="800">
+                Your Web & Mobile App Development Partner - Empowered By
+                Innovation
+              </h1>
+              <div data-aos="fade-up" data-aos-duration="1000">
+                <div className="letwork">
+                  <Link to="/contact" className={loaded ? "loaded-link" : ""}>
+                    Let’s work together
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-11">
+            <div
+              className="HeroVideo_video"
+              data-aos="fade-up"
+              data-aos-duration="500"
+            >
+              <video
+                muted
+                playsInline
+                crossOrigin
+                loop
+                poster
+                preload="auto"
+                autoPlay
+              >
+                <source src="videos/Webpoint Reel.webm" type="video/webm" />
+              </video>
             </div>
           </div>
         </div>
-        <div className="col-lg-11">
-          <div
-            className="HeroVideo_video"
-            data-aos="fade-up"
-            data-aos-duration="500"
-          >
-            <video
-              muted
-              playsInline
-              crossOrigin
-              loop
-              poster
-              preload="auto"
-              autoPlay
-            >
-              <source src="videos/Webpoint Reel.webm" type="video/webm" />
-            </video>
-          </div>
-        </div>
       </div>
-    </div>
-  </section>
-)}
+    </section>
+  )
+}
 
 export default HeroVideo
